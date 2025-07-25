a0fa6cf7e93424bd18f3593ffd3487d74b373140
Pál András Bence <palandrasbence@gmail.com>
2025-07-25 14:50:10 +0200

    Add Expense and Income API controllers with CRUD operations

---
+++ BACKEND/BACKEND/Controllers/ExpenseApiController.cs
@@ -0,0 +1,48 @@
+﻿using BACKEND.Data;
+using BACKEND.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BACKEND.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExpenseController : ControllerBase
+    {
+        IExpenseRepository repository;
+
+        public ExpenseController(IExpenseRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Expense> GetExpenses()
+        {
+            return this.repository.Read();
+        }
+
+        [HttpGet("{id}")]
+        public Expense? GetExpense(int id)
+        {
+            return this.repository.Read(id);
+        }
+
+        [HttpPost]
+        public void CreateExpense([FromBody] Expense expense)
+        {
+            this.repository.Create(expense);
+        }
+
+        [HttpPut]
+        public void UpdateExpense([FromBody] Expense expense)
+        {
+            this.repository.Update(expense);
+        }
+
+        [HttpDelete("{id}")]
+        public void DeleteExpense(int id)
+        {
+            this.repository.Delete(id);
+        }
+    }
+}
---
+++ BACKEND/BACKEND/Controllers/IncomeApiController.cs
@@ -0,0 +1,48 @@
+﻿using BACKEND.Data;
+using BACKEND.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BACKEND.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IncomeController : ControllerBase
+    {
+        IIncomeRepository repository;
+
+        public IncomeController(IIncomeRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet]
+        public IEnumerable<Income> GetIncomes()
+        {
+            return this.repository.Read();
+        }
+
+        [HttpGet("{id}")]
+        public Income? GetIncome(int id)
+        {
+            return this.repository.Read(id);
+        }
+
+        [HttpPost]
+        public void CreateIncome([FromBody] Income income)
+        {
+            this.repository.Create(income);
+        }
+
+        [HttpPut]
+        public void UpdateIncome([FromBody] Income income)
+        {
+            this.repository.Update(income);
+        }
+
+        [HttpDelete("{id}")]
+        public void DeleteIncome(int id)
+        {
+            this.repository.Delete(id);
+        }
+    }
+}
