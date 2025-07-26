39419963ef26bf7de7b9d74012887437b193a267
Pál András Bence <palandrasbence@gmail.com>
2025-07-26 12:00:39 +0200

    Add EnumController and update route definitions for Expense and Income controllers

---
+++ BACKEND/BACKEND/Controllers/EnumApiController.cs
@@ -0,0 +1,36 @@
+﻿using BACKEND.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace BACKEND.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EnumController
+    {
+        [HttpGet("expense-categories")]
+        public Dictionary<int, string> GetExpenseCategories()
+        {
+            Dictionary<int, string> categories = Enum.GetValues(typeof(ExpenseCategory))
+                .Cast<ExpenseCategory>()
+                .ToDictionary(
+                    e => (int)e,
+                    e => CultureInfo.CurrentCulture.TextInfo.ToTitleCase(e.ToString())
+                );
+            return categories;
+        }
+
+        [HttpGet("income-categories")]
+        public Dictionary<int, string> GetIncomeCategories()
+        {
+            Dictionary<int, string> categories = Enum.GetValues(typeof(IncomeCategory))
+                .Cast<IncomeCategory>()
+                .ToDictionary(
+                    e => (int)e,
+                    e => CultureInfo.CurrentCulture.TextInfo.ToTitleCase(e.ToString())
+                );
+            return categories;
+        }
+    }
+}
--- BACKEND/BACKEND/Controllers/IncomeApiController.cs
+++ BACKEND/BACKEND/Controllers/IncomeApiController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace BACKEND.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class IncomeController : ControllerBase
     {
         IIncomeRepository repository;
