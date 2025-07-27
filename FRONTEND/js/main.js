ed0611cd0827828a1c4df8b2a23b70fae9810b65
Pál András Bence <palandrasbence@gmail.com>
2025-07-27 19:24:55 +0200

    Refactor category fetching and display logic for income and expenses

--- FRONTEND/js/main.js
+++ FRONTEND/js/main.js
@@ -1,23 +1,15 @@
-const expenseCategoryEnum = {};
 const incomeCategoryEnum = {};
+const expenseCategoryEnum = {};
 
 async function fetchCategories() {
-    const response1 = await fetch('http://localhost:5152/api/enum/expense-categories');
-    const expenseCategories = await response1.json();
-    Object.assign(expenseCategoryEnum, expenseCategories);
-    const response2 = await fetch('http://localhost:5152/api/enum/income-categories');
-    const incomeCategories = await response2.json();
+    const response1 = await fetch('http://localhost:5152/api/enum/income-categories');
+    const incomeCategories = await response1.json();
     Object.assign(incomeCategoryEnum, incomeCategories);
 
-    document.querySelector('#expense-category').innerHTML = '';
-    for (const key in expenseCategoryEnum) {
-        if (Object.hasOwnProperty.call(expenseCategoryEnum, key)) {
-            const option = document.createElement('option');
-            option.value = key;
-            option.innerText = expenseCategoryEnum[key];
-            document.querySelector('#expense-category').appendChild(option);
-        }
-    }
+    const response2 = await fetch('http://localhost:5152/api/enum/expense-categories');
+    const expenseCategories = await response2.json();
+    Object.assign(expenseCategoryEnum, expenseCategories);
+
     document.querySelector('#income-category').innerHTML = '';
     for (const key in incomeCategoryEnum) {
         if (Object.hasOwnProperty.call(incomeCategoryEnum, key)) {
@@ -25,6 +17,16 @@ async function fetchCategories() {
             option.value = key;
             option.innerText = incomeCategoryEnum[key];
             document.querySelector('#income-category').appendChild(option);
+        }
+    }
+    
+    document.querySelector('#expense-category').innerHTML = '';
+    for (const key in expenseCategoryEnum) {
+        if (Object.hasOwnProperty.call(expenseCategoryEnum, key)) {
+            const option = document.createElement('option');
+            option.value = key;
+            option.innerText = expenseCategoryEnum[key];
+            document.querySelector('#expense-category').appendChild(option);
         }
     }
 }
@@ -35,6 +37,9 @@ async function downloadAndDisplay() {
 
     const response2 = await fetch('http://localhost:5152/api/expense');
     const expenses = await response2.json();
+
+    document.querySelector('#income-all').innerHTML = '';
+    document.querySelector('#expense-all').innerHTML = '';
 
     if (incomes.length > 0 || document.querySelectorAll('#income-list .budget__table-empty').length === 0) {
         document.querySelector('#income-list').innerHTML = '';
@@ -60,7 +65,32 @@ async function downloadAndDisplay() {
         tr.appendChild(td);
         document.querySelector('#expense-list').appendChild(tr);
     }
+    
+    let incomeId = 1;
+    incomes.map(income => {
+        let tr = document.createElement('tr');
+        let tdID = document.createElement('td');
+        let tdCategory = document.createElement('td');
+        let tdAmount = document.createElement('td');
+        let tdDate = document.createElement('td');
+        let tdAction = document.createElement('td');
 
+        tdID.innerText = incomeId++;
+        tdCategory.innerText = incomeCategoryEnum[income.category];
+        tdAmount.innerText = income.amount + '€';
+        tdDate.innerText = new Date(income.date).toLocaleDateString('en-GB');
+        tdAction.innerHTML = `<button class="budget__button budget__button--delete" onclick="deleteIncome(${income.id})">Delete</button>`;
+
+        tr.appendChild(tdID);
+        tr.appendChild(tdCategory);
+        tr.appendChild(tdAmount);
+        tr.appendChild(tdDate);
+        tr.appendChild(tdAction);
+
+        document.querySelector('#income-list').appendChild(tr);
+    });
+
+    let expenseId = 1;
     expenses.map(expense => {
         let tr = document.createElement('tr');
         let tdID = document.createElement('td');
@@ -69,7 +99,7 @@ async function downloadAndDisplay() {
         let tdDate = document.createElement('td');
         let tdAction = document.createElement('td');
 
-        tdID.innerText = expense.id;
+        tdID.innerText = expenseId++;
         tdCategory.innerText = expenseCategoryEnum[expense.category];
         tdAmount.innerText = expense.amount + '€';
         tdDate.innerText = new Date(expense.date).toLocaleDateString('en-GB');
@@ -84,28 +114,33 @@ async function downloadAndDisplay() {
         document.querySelector('#expense-list').appendChild(tr);
     });
 
-    incomes.map(income => {
-        let tr = document.createElement('tr');
-        let tdID = document.createElement('td');
-        let tdCategory = document.createElement('td');
-        let tdAmount = document.createElement('td');
-        let tdDate = document.createElement('td');
-        let tdAction = document.createElement('td');
+    let sumAmounts = arr => arr.reduce((sum, item) => sum + item.amount, 0);
+    let allIncome = sumAmounts(incomes);
+    let allExpense = sumAmounts(expenses);
+    let grossCashFlow = allIncome + allExpense;
 
-        tdID.innerText = income.id;
-        tdCategory.innerText = incomeCategoryEnum[income.category];
-        tdAmount.innerText = income.amount + '€';
-        tdDate.innerText = new Date(income.date).toLocaleDateString('en-GB');
-        tdAction.innerHTML = `<button class="budget__button budget__button--delete" onclick="deleteIncome(${income.id})">Delete</button>`;
+    let percantageIncome = Math.round((allIncome / grossCashFlow) * 100, 0) || 0;
+    let percantageExpense = Math.round((allExpense / grossCashFlow) * 100, 0) || 0;
+    let degreeIncome = percantageIncome * 3.6;
+    let degreeExpense = percantageExpense * 3.6;
 
-        tr.appendChild(tdID);
-        tr.appendChild(tdCategory);
-        tr.appendChild(tdAmount);
-        tr.appendChild(tdDate);
-        tr.appendChild(tdAction);
+    document.querySelector('#income-bar-text').innerHTML = allIncome + '€';
+    document.querySelector('#expense-bar-text').innerHTML = allExpense + '€';
 
-        document.querySelector('#income-list').appendChild(tr);
-    });
+    document.querySelector('#income-all').innerHTML += allIncome + '€';
+    document.querySelector('#expense-all').innerHTML += allExpense + '€';
+    document.querySelector('#total').innerHTML = (allIncome - allExpense) + '€';
+    
+    document.querySelector('#income-percentage').innerHTML = percantageIncome + '%';
+    document.querySelector('#expense-percentage').innerHTML = percantageExpense + '%';
+
+    document.querySelector('#piechart').style.backgroundImage = 
+        'conic-gradient(' +
+        'var(--color-danger) 0deg ' + degreeExpense + 'deg, ' +
+        'var(--color-success) ' + degreeExpense + 'deg 360deg)';
+
+    document.querySelector('#income-bar').style.height = (percantageIncome * 2) + 'px';
+    document.querySelector('#expense-bar').style.height = (percantageExpense * 2) + 'px';
 }
 
 fetchCategories();
